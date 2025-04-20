63db89dc9d9d823bea8d6f3b49e4588f2734bbd4
Michael Cardoso <michaelcardoso97@gmail.com>
2025-04-20 08:00:00 -0300

    Exibindo pensamento na dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,7 +7,19 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render("toughts/dashboard");
+    const userId = req.session.userid;
+    const user = await User.findOne({
+      where: { id: userId },
+      include: Tought,
+      plain: true,
+    });
+
+    const toughts = user.Toughts.map((result)=>result.dataValues)
+    // console.log(toughts)
+    if (!user) {
+      res.redirect("/login");
+    }
+    res.render("toughts/dashboard",{toughts});
   }
 
   static createTought(req, res) {

be7f1e10b752e4a67fabf761965a2b8d73398eba
Michael Cardoso <michaelcardoso97@gmail.com>
2025-04-21 09:25:16 -0300

    Ordenaçãao de exibição

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -9,11 +9,21 @@ module.exports = class ToughtController {
     if (req.query.search) {
       search = req.query.search;
     }
+
+    let order = "DESC";
+
+    if (req.query.order === "old") {
+      order = "ASC";
+    } else {
+      order = "DESC";
+    }
+
     const toughtsData = await Tought.findAll({
       include: User,
       where: {
         title: { [Op.like]: `%${search}%` },
       },
+      order: [["createdAt", order]],
     });
 
     const toughts = toughtsData.map((result) => result.get({ plain: true }));
